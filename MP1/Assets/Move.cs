fd640281c98baf20dc2e3f166779df605fc12ad1
Youssef Beltagy <potoato@hotmail.com>
2021-10-09 22:40:42 -0700

    Fixed bug where eggs would freeze before returning to their position

--- MP1/Assets/Move.cs
+++ MP1/Assets/Move.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-
-    public Vector2 step = new Vector2(0, 0);
+    public Vector2 start_pos = new Vector2(1, 1);
+    public Vector2 step = new Vector2(1, 1); // should never have x = 0
     public Vector2 min_val = new Vector2(0, 0);
-    public Vector2 max_val = new Vector2(0, 0);
+    public Vector2 max_val = new Vector2(50, 50);
+    
 
     void FixedUpdate()
     {
         float v_val = Input.GetAxis("Vertical");
-        if(v_val != 0) ExpandCollapse(v_val); // 0 is set, so it is okay to compare with it.
+        if(Mathf.Abs(v_val) >= 0.1) ExpandCollapse(v_val);
  
     }
 
@@ -23,28 +24,29 @@ public class Move : MonoBehaviour
     /// <param name="v_val">The Vertical Axis reading</param>
     private void ExpandCollapse(float v_val)
     {
+
         // using v_val as a weight adds inertia to the movement and guarantees that
         // The movement and size of the GameObject are both functions of V_Val. Thus,
         // At any position, there can only be one size.
+        float x = transform.position.x + step.x * v_val; // Find the next x postion
+
+        // Clamp the next  xposition and then find the difference between the next position and current position
+        x = Mathf.Max(Mathf.Min(x, max_val.x), min_val.x) - transform.position.x;
+
+        v_val = x / step.x; // find the movement factor.
+
         Vector2 increment = step * v_val;
-
-        if (
-            !( // If the next step is not in range, do nothing.
-            ((transform.position.x + increment.x) <= max_val.x) &&
-            ((transform.position.x + increment.x) >= min_val.x) &&
-            ((transform.position.y + increment.y) <= max_val.y) &&
-            ((transform.position.y + increment.y) >= min_val.y)
-            )
-            
-            ) return;
-
         transform.position += new Vector3(increment.x, increment.y, 0);
 
         float s = v_val * 0.1f;
         Vector3 size = new Vector3(s, s, 0);
-        Vector3 sum = transform.localScale + size;
-        if (sum.x >= 0 && sum.y >= 0)
-            transform.localScale += size;
+        transform.localScale += size;
+
+        if(transform.position.x == start_pos.x || transform.position.y == start_pos.y) // reset the values;
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            transform.position = new Vector3(start_pos.x, start_pos.y, 0);
+        }
 
     }
 
